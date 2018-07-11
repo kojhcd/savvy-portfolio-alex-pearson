992a63a7dd92e08d0617e1f72ab777eda1fd653b
Alex Pearson <alex@alexpear.com>
2018-07-10 21:23:57 -0500

    Add SPA-style navigation

--- index.js
+++ index.js
@@ -3,34 +3,77 @@ import Footer from './src/Footer';
 import Header from './src/Header';
 import Navigation from './src/Navigation';
 
+var Home = {
+    'title': 'Alex Pearson\'s Project'
+};
 
-var title = 'Alex Pearson\'s Project';
+var Blog = {
+    'title': 'Welcome to my blog page'
+};
 
-function greetUser(){
-    var greeting = document.querySelector('#greeting');
-    var firstName = prompt('What is your first name?');
-    var lastName = prompt('What is your last name?');
+var Contact = {
+    'title': 'Contact Me'
+};
 
-    if(firstName && lastName){
-        greeting.innerHTML = `
-          <div>
+var Projects = {
+    'title': 'Take a look at some of my projects'
+};
+
+var root = document.querySelector('#root');
+
+function render(state){
+    var greeting;
+    var input;
+    var links;
+
+    root.innerHTML = `
+        ${Navigation}
+        ${Header(state)}
+        ${Content}
+        ${Footer}
+    `;
+
+    greeting = document.querySelector('#greeting');
+    input = document.querySelector('#header input');
+
+    input.addEventListener(
+        'keyup',
+        (event) => greeting.innerHTML = `
+        <div>
             <h3>Welcome to my home page,</h3>
-            <h4>${firstName} ${lastName}</h4>
-          </div>
-        `;
-    }
-    else{
-        greetUser();
-    }
+            <h4>${event.target.value}</h4>
+        </div>
+    `
+    );
+
+    links = document.querySelectorAll('#navigation a');
+
+    links[0].addEventListener(
+        'click',
+        (event) => {
+            event.preventDefault();
+
+            render(Blog);
+        }
+    );
+
+    links[1].addEventListener(
+        'click',
+        (event) => {
+            event.preventDefault();
+
+            render(Contact);
+        }
+    );
+
+    links[2].addEventListener(
+        'click',
+        (event) => {
+            event.preventDefault();
+
+            render(Projects);
+        }
+    );
 }
 
-document
-    .querySelector('#root')
-    .innerHTML = `
-    ${Navigation}
-    ${Header(title)}
-    ${Content}
-    ${Footer}
-  `;
-
-greetUser();
+render(Home);
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,13 @@
 import Hair from '../assets/hairdo.jpg';
 
-export default function Header(title){
+export default function Header(state){
     return `
     <div id="header" class="hotpink">
       <div class="container">
-        <h1 class="enpinkify">${title}</h1>
+        <h1 class="enpinkify">${state.title}</h1>
         <img src="${Hair}" alt="my face">
         <div id="greeting"></div>
+        <input type="text">
       </div>
     </div>
   `;
